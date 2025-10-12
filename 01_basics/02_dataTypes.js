5c277052feca55fb703d870755de5d6236ba32b9
gauravcoder11 <pathakgaurav3004@gmail.com>
2025-10-12 07:11:18 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,41 @@
+console.log(3+3)
+
+//String
+let name = "Gaurav"
+//integer
+let age = 19
+//boolean
+let isLoggedIn = false
+
+//range of number => 2 to power 53
+//bigint
+//String => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+console.log(typeof"gaurav")//return string
+
+console.log(typeof age)//return number
+
+console.log(typeof null)//return object
+
+console.log(typeof undefined)//return undefined
+
+console.log(typeof 42);            // "number"
+
+console.log(typeof 'Jarvas');      // "string"
+
+console.log(typeof true);          // "boolean"
+
+console.log(typeof undefined);     // "undefined"
+
+console.log(typeof {name: 'Bruno'}); // "object"
+
+console.log(typeof [1, 2, 3]);     // "object"  ← Arrays are objects!
+
+console.log(typeof function(){});  // "function"
+
+console.log(typeof null);          // "object"  ← a historical bug in JS
+
