f90070da168bb3d3fbcf6903b68cc5f4642f6fe1
gauravcoder11 <pathakgaurav3004@gmail.com>
2025-10-12 10:50:44 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,74 @@
+// let score = 300
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let total = "30ab"
+
+let valueInNumber = Number(total)
+
+console.log(typeof valueInNumber)
+
+let check1 = null
+
+let conversion = Number(check1)
+console.log(typeof(conversion))
+console.log(conversion)
+
+let check2 = undefined
+
+let conversion2 = Number(check2)
+console.log(typeof(conversion2))
+console.log(conversion2)
+
+let check3 = "22ab"
+
+let conversion3 = Number(check3)
+console.log(typeof(conversion3))
+console.log(conversion3)
+
+let check4 = true
+
+let conversion4 = Number(check4)
+console.log(typeof(conversion4))
+console.log(conversion4)
+
+//After conversion result will be
+//"33" => 33
+//"33ab" => NaN
+//true => 1; false => 0
+
+
+//Boolean conversion
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let a =""
+
+let conv = Boolean(a)
+console.log(conv)
+
+let b ="Gaurav"
+
+let conv2 = Boolean(b)
+console.log(conv2)
+
+let c = undefined
+
+let conv3 = Boolean(c)
+console.log(conv3)
+
+//1 => true; 0 => false;
+//"" => false;
+//"gaurav" => true
+//null => false
+//undefined => false;
+
+//String Conversion
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
