6ed19d8d5d61b2fd5c2ad64af21c568437da4559
gauravcoder11 <pathakgaurav3004@gmail.com>
2025-10-16 11:12:00 +0000

    some additional conversion

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,3 +1,31 @@
+//null comparison
+// console.log(null<=0)
+// console.log(null==0)
+// console.log(null>0)
+
+//undefined comparison
+console.log(null == undefined)
+console.log(undefined < null)
+console.log(undefined > null)
+console.log(undefined==0)
+console.log(undefined>=0)
+//boolean comparison
+// console.log(true==1)
+// console.log(true==0)
+// console.log(false==1)
+// console.log(false==0)
+
+//String comparison
+// console.log("02">0)
+// console.log("02"<0)
+// console.log("02"==2)
+// console.log("2">1)
+
+// console.log("1"==true)
+// console.log("2"===2)
+
 console.log(null<=0)
-console.log(null==0)
-console.log(null>0)
+
+console.log(null == undefined)
+
+console.log("2"===2)
