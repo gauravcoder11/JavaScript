69c632f78f4cc7e9042dfa37f457527809a91c72
gauravcoder11 <pathakgaurav3004@gmail.com>
2025-10-19 09:07:18 +0000

    stack and heap memory in js

--- 01_basics/05_dataTypes_summary.js
+++ 01_basics/05_dataTypes_summary.js
@@ -36,3 +36,34 @@ console.log(typeof myFunction)
 const outsideTemp = null
 console.log(typeof outsideTemp)
 
+//********************Memory********************
+//Stack (Primitive)
+//Heap (Non-Primitive)
+
+let name = "gaurav"
+let anotherName = name 
+anotherName = "saurabh"
+console.log(name)
+console.log(anotherName)
+
+/* It will print gaurav and saurabh because anotherName
+will store the copy of name in stack */
+
+let userOne ={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "gaurav@google.com"
+console.log(userOne.email)
+console.log(userTwo.email)
+
+/*both will print the same value because they will point
+the same reference in memory in heap if we change the one
+value both will be changed*/  
+
+
+
+
